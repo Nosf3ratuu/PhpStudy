47b5e9a94bbe6f3fc34075b6c845c3e9013be7a0
Alex Brown <alexander@wombat.me>
2025-03-30 21:52:00 +0100

    starting essentials

---
+++ phpEssentials/strings/strings1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./simple.css" />
+    <title>Document</title>
+</head>
+<body>
+    <pre><?php
+
+$greeting = "I'm learning php";
+echo "{$greeting}_!";
+
+echo '<br />';
+$name = 'Alex';
+$subject = 'php';
+
+echo "My name is {$name}, and I am learning {$subject}";
+echo "\n";
+echo "\t -";
+    ?></pre>
+</body>
+</html>
